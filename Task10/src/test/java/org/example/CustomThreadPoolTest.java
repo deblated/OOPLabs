d5dec367796ffaf697c940e9d6eb404ffc9dad9c
deblated <bober.vasia@gmail.com>
2022-12-13 19:19:33 +0200

    Reduced time of task "execution" in task 10

--- Task10/src/test/java/org/example/CustomThreadPoolTest.java
+++ Task10/src/test/java/org/example/CustomThreadPoolTest.java
@@ -12,21 +12,21 @@ class CustomThreadPoolTest {
 
         CustomThreadPool pool = new CustomThreadPool(4);
 
-        pool.execute(new CustomTask("0", 300L,testClass));
-        pool.execute(new CustomTask("1", 300L,testClass));
-        pool.execute(new CustomTask("2", 500L,testClass));
-        pool.execute(new CustomTask("3", 500L,testClass));
-        pool.execute(new CustomTask("4", 700L,testClass));
-        pool.execute(new CustomTask("5", 700L,testClass));
+        pool.execute(new CustomTask("0", 30L,testClass));
+        pool.execute(new CustomTask("1", 30L,testClass));
+        pool.execute(new CustomTask("2", 50L,testClass));
+        pool.execute(new CustomTask("3", 50L,testClass));
+        pool.execute(new CustomTask("4", 70L,testClass));
+        pool.execute(new CustomTask("5", 70L,testClass));
 
         pool.shutdown();
 
-        pool.execute(new CustomTask("6", 700L,testClass));
+        pool.execute(new CustomTask("6", 70L,testClass));
 
         Thread waitThread = new Thread(() ->
         {
             try {
-                Thread.sleep(1500L);
+                Thread.sleep(500L);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
